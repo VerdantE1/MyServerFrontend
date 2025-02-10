60f1796b4f6d7cfdb42b131f26e5ccb3fc053d86
Frank <2523877046@qq.com>
2025-02-10 19:30:48 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import Navbar from './components/Navbar.vue'
+  import Person from './components/Person.vue'
+</script>
+
+<template>
+  <div class="app">
+    <h1>Hello World!</h1>
+    <Navbar/>
+  </div>
+
+  <Person/>
+
+
+</template>
+
+<style scoped>
+  .app{
+    background: #ddd;
+    box-shadow:0 0 20px;
+    border-radius:10px;
+    padding: 20px;
+  }
+</style>
